813d842a60c3db7693ea6c39d9aec664d925e179
lewelyn7 <progit8itakademia>
2019-12-21 16:42:29 +0100

    after cleaning

--- src/assets/MultiMap.java
+++ src/assets/MultiMap.java
@@ -23,66 +23,6 @@ public class MultiMap<K, V> extends HashMap<K, PriorityQueue<V>>{
             this.remove(key);
         }
     }
-//
-//    @Override
-//    public Iterator<V> iterator(){
-//        throw new UnsupportedOperationException("multipmap iterator");
-//        //return (new MultiMapIterator<K,V>(this));
-//    }
-
 
 }
 
-//class MultiMapIterator<K, V> implements Iterator<V>{
-//
-//    MultiMap<K, V> map;
-//    Set<Map.Entry<K, PriorityQueue<V>>> mapSet;
-//    Iterator<Map.Entry<K,PriorityQueue<V>>> setIterator;
-//    Iterator<V> queueIterator;
-//    V toReturn;
-//    PriorityQueue<V> queue;
-//    boolean endOfQueue = true;
-//
-//    public MultiMapIterator(MultiMap map) {
-//        this.map = map;
-//        mapSet = map.entrySet();
-//        setIterator = mapSet.iterator();
-//        if(setIterator.hasNext()) {
-//            queueIterator = setIterator.next().getValue().iterator();
-//            if (queueIterator.hasNext()) endOfQueue = false;
-//            else endOfQueue = true;
-//        }
-//
-//    }
-//
-//
-//
-//
-//    @Override
-//    public boolean hasNext() {
-//        if(endOfQueue) {
-//            if (!setIterator.hasNext()) return false;
-//            queue = setIterator.next().getValue();
-//            queueIterator = queue.iterator();
-//            endOfQueue = false;
-//            return (queueIterator.hasNext());
-//        }else{
-//            if(queueIterator.hasNext()){
-//                return true;
-//            }else{
-//                return false;
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public V next() {
-//            toReturn = queueIterator.next();
-//            if(!queueIterator.hasNext()) endOfQueue = true;
-//            return toReturn;
-//    }
-//    @Override
-//    public void remove(){
-//        queue.remove(toReturn);
-//    }
-//}
